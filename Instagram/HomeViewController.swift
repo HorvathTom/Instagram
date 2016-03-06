702b41f85306b498f3fd7f56e0d3e792e6889719
Tom Horvath <thorvath@mail.ccsf.edu>
2016-03-05 19:41:13 -0800

    Implement logout

---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by Tom H on 3/5/16.
+//  Copyright © 2016 Tom Horvath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onLogout(sender: AnyObject) {
+        PFUser.logOut()
+    
+        self.dismissViewControllerAnimated(true, completion: {})
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
