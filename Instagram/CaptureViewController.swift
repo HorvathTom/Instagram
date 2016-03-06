a4ec59b92b70dde613c1d8f6c985e302339817a5
Tom Horvath <thorvath@mail.ccsf.edu>
2016-03-06 14:24:38 -0800

    Implement capture view controller

--- Instagram/CaptureViewController.swift
+++ Instagram/CaptureViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class CaptureViewController: UIViewController {
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var captionField: UITextField!
+    @IBOutlet weak var selectPhotoButton: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +26,48 @@ class CaptureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func selectPhotoAction(sender: AnyObject) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // Get the image captured by the UIImagePickerController
+        photoImageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        photoImageView.hidden = false
+        
+        submitButton.hidden = false
+        selectPhotoButton.hidden = true
+            
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func submitAction(sender: AnyObject) {
+        Post.postUserImage(photoImageView.image, withCaption: captionField.text) { (success: Bool, error: NSError?) -> Void in
+            if success {
+                print("Submitted user image to parse")
+                
+                // Reset Capture tab state
+                self.photoImageView.image = nil
+                self.photoImageView.hidden = true
+                self.submitButton.hidden = true
+                self.selectPhotoButton.hidden = false
+            
+                // Go to Home tab
+                self.tabBarController?.selectedIndex = 0
+            } else {
+                print("Error submitting user image to parse")
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
